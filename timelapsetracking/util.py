d1d9b4dd125fad0256846b7c2673c044eef5165b
Chek Ounkomol <chek.o@outlook.com>
2019-03-27 11:26:46 -0700

    Fix formating and add image finder utility function

--- timelapsetracking/util.py
+++ timelapsetracking/util.py
@@ -1,3 +1,8 @@
+"""General utility functions."""
+
+
+from typing import List, Sequence
+import os
 import time
 
 
@@ -11,3 +16,31 @@ def report_run_time(fn):
         print(f'{name} executed in {toc - tic:.2f} s.')
         return retval
     return wrapper
+
+
+def images_from_dir(
+        path_dir: str,
+        extensions: Sequence[str] = ('.tif', '.tiff'),
+) -> List[str]:
+    """Returns a sorted list of images found in the input directory.
+
+    Parameters
+    ----------
+    path_dir
+        Path to input directory.
+    extensions
+        File extensions to identify a file as an image.
+
+    Returns
+    -------
+    List[str]
+        Sorted list of paths to images in input directory.
+
+    """
+    paths_img = sorted(
+        [
+            p.path for p in os.scandir(path_dir)
+            if any(ext in p.path.lower() for ext in extensions)
+        ]
+    )
+    return paths_img
