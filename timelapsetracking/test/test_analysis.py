11dafddb38d5537b8294e2e98e734681d96f58ba
Chek Ounkomol <cheko@alleninstitute.org>
2019-04-15 11:03:12 -0700

    Update add_delta_pos test to include test of malformed input

--- timelapsetracking/test/test_analysis.py
+++ timelapsetracking/test/test_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
+import pytest
 
 from timelapsetracking.tracks.analysis import add_delta_pos
 
@@ -10,12 +11,16 @@ def test_add_delta_pos():
     df_i = pd.DataFrame(
         {
             'node_id': range(9),
-            'in_list': [None, 0, 1, None, 3, 4, None, 6, None],
             'centroid_x': [1, 2, 3.1, 4, 6, 8.2, 9, 12, 666],
             'centroid_y': [2, 2, 2, 3, 3, 3, -3, -3, 777],
             'centroid_z': [0, -1, -3, 0, 3, 7, 0, -5, 888],
         }
     ).set_index('node_id')
+    # Check for exception if DataFrame lacks 'in_list' column
+    with pytest.raises(ValueError):
+        df_o = add_delta_pos(df_i)
+
+    df_i = df_i.assign(in_list=[None, 0, 1, None, 3, 4, None, 6, None])
     df_o = add_delta_pos(df_i)
     delta_x_exp = [np.nan, 1.0, 1.1, np.nan, 2.0, 2.2, np.nan, 3.0, np.nan]
     delta_y_exp = [np.nan, 0.0, 0.0, np.nan, 0.0, 0.0, np.nan, 0.0, np.nan]
