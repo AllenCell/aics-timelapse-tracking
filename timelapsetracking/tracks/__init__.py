8a78e3b4644ebc1c16a85e3ccf2dae4330f30473
Chek Ounkomol <cheko@alleninstitute.org>
2019-06-06 16:39:21 -0700

    Update connectivity module so main function annotates nodes with lineage_id and track_id

--- timelapsetracking/tracks/__init__.py
+++ timelapsetracking/tracks/__init__.py
@@ -1,10 +1,10 @@
 from timelapsetracking.tracks.analysis import calc_track_lengths
-from timelapsetracking.tracks.connectivity import add_track_ids
+from timelapsetracking.tracks.connectivity import add_connectivity_labels
 from timelapsetracking.tracks.edges import add_edges
 
 
 __all__ = [
+    'add_connectivity_labels',
     'add_edges',
-    'add_track_ids',
     'calc_track_lengths',
 ]
