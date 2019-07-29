5d3899a0872c935f3df7ea10c27c4544f8b67fed
Chek Ounkomol <cheko@alleninstitute.org>
2019-07-29 11:56:18 -0700

    Update track length calculator to use either track_id or lineage_id

--- timelapsetracking/__init__.py
+++ timelapsetracking/__init__.py
@@ -1,8 +1,7 @@
-from timelapsetracking.dir_to_nodes import dir_to_nodes
 from timelapsetracking.version import MODULE_VERSION
 
 
-__all__ = ['dir_to_nodes', 'MODULE_VERSION']
+__all__ = ['MODULE_VERSION']
 __version__ = MODULE_VERSION
 
 
